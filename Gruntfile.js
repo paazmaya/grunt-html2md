f8d013fda84601cec8dd4dd4e5df8803cec6e91c
Juga Paazmaya <olavic@gmail.com>
2014-04-16 23:28:35 +0300

    Unify configuration between my Node.js based projects

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,13 @@
-/*
+/**
  * grunt-html2md
  * https://github.com/paazmaya/grunt-html2md
  *
- * Copyright (c) 2013 Juga Paazmaya
+ * Copyright (c) Juga Paazmaya <olavic@gmail.com>
  * Licensed under the MIT license.
  */
+'use strict';
+  
 module.exports = function(grunt) {
-  'use strict';
 
   // Project configuration.
   grunt.initConfig({
@@ -45,19 +46,13 @@ module.exports = function(grunt) {
 
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'eslint', 'html2md', 'nodeunit']);
 
-  // By default, run all tests.
   grunt.registerTask('default', ['test']);
-
 };
