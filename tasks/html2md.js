b175e3f60292f2a03e5c981d460c230cdde499f4
Juga Paazmaya <olavic@gmail.com>
2014-07-10 12:03:08 +0300

    ESLint upgrade with more validation options for better JavaScript

--- tasks/html2md.js
+++ tasks/html2md.js
@@ -1,35 +1,36 @@
-/*
+/**
  * grunt-html2md
  * https://github.com/paazmaya/grunt-html2md
  *
- * Copyright (c) 2013 Juga Paazmaya
+ * Copyright (c) Juga Paazmaya
  * Licensed under the MIT license.
  */
-module.exports = function (grunt) {
-  'use strict';
+'use strict';
 
-  grunt.registerMultiTask('html2md', 'Transform HTML files to Markdown', function() {
-    
+module.exports = function html2md(grunt) {
+
+  grunt.registerMultiTask('html2md', 'Transform HTML files to Markdown', function register() {
+
     var toMarkdown = require('to-markdown').toMarkdown;
     var options = this.options({
       encoding: 'utf8'
     });
-    
+
     // Iterate over all specified file groups.
-    this.filesSrc.forEach(function(filepath) {
-    
-      // Warn on and remove invalid source files (if nonull was set).
+    this.filesSrc.forEach(function filesEach(filepath) {
+
+      // Warn on and remove invalid source files (if no null was set).
       if (!grunt.file.exists(filepath) || !grunt.file.isFile(filepath)) {
         grunt.log.warn('Source file "' + filepath + '" not found.');
       }
       else {
         // Read file source.
         var html = grunt.file.read(filepath);
-        
+
         // Convert
         var md = toMarkdown(html);
-      
-        // Replace suffix of of src to create dest
+
+        // Replace suffix of source to create destination
         var dest = filepath.substring(0, filepath.lastIndexOf('.')) + '.md';
 
         // Write the destination file.
